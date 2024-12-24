d1eeb9700bb6539a6bb5079f5b0372a06c6c4f1a
unknown <brankliz7@gmail.comgit config --global user.email brankliz7@gmail.com>
2024-12-24 09:30:44 +0300

    3.1

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,51 +15,29 @@ class User extends Authenticatable
     const ROLE_ADMIN = 'admin';
     const ROLE_VALUATOR = 'valuator';
     const ROLE_EXTERNAL = 'external';
-
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name', 'email', 'password', 'role',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role',];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var list<string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password','remember_token',];
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected function casts(): array{
+        return ['email_verified_at' => 'datetime','password' => 'hashed',];}
      // A user can have many assets
-     public function assets()
-     {
-         return $this->hasMany(Asset::class);
-     }
- 
+     public function assets() {return $this->hasMany(Asset::class);}
      // A user can be a valuator for many valuations
-     public function valuations()
-     {
-         return $this->hasMany(Valuation::class, 'valuator_id');
-     }
-     public function roles()
-     {
-         return $this->belongsToMany(Role::class);
-     }
-}
+     public function valuations(){return $this->hasMany(Valuation::class, 'valuator_id');}
+     public function roles(){return $this->belongsToMany(Role::class);}}
